6d8286fcc6c9cae89a5af57998aed63e0db9e573
atishpanda <atishpanda2@gmail.com>
2022-06-02 18:44:55 +0530

    Contract skeleton

--- contract-basic 1.sol
+++ contract-basic 1.sol
@@ -1,7 +1,7 @@
 // Basic contract skeleton
 
 // We are setting the solidity compiler version to anything in between 0.5.0 and 0.6.0
-// contract will only be executed when solidity compliler falls between this version 
+// contract will only be executed when solidity compliler falls in between the above set version 
 pragma solidity >=0.5.0 <0.6.0; 
 
 // empty contract 
